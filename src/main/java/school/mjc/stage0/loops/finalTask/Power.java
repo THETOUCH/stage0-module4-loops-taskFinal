ff7556648ba50266ecdb350404e17919d24aeffb
THETOUCH <super.ilgiznex@gmail.com>
2023-10-20 21:30:26 +0600

    123

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,15 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int a, b, c;
+        a = numberToPrint;
+        b = power;
+        c = 0;
+        int sum = 1;
+        for (int i = 0; i < b; i++) {
+            sum = sum * a;
+        }
+        System.out.println(sum);
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,21 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int z = 0; z < cathetusLength - i; z++) {
+                System.out.print(" ");
+            }
+//            for (int j = 1; j<=i; j++) {
+//                System.out.print(j);
+//            }
+            for(int j = i; j !=0; j--) {
+                System.out.print(j);
+            }
+            for (int k=2;k<=i;k++) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
